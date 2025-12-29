e332fd6b90c7ee42c8b00dfd4b72afed3fe79aa7
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-12-29 01:38:14 +0000

    Create objects.ts

---
+++ objects.ts
@@ -0,0 +1,13 @@
+const car: {
+    name: string,
+    brand: string,
+    year: number
+} = {
+    name: 'Fit',
+    brand: 'Honda',
+    year: 2005
+};
+
+console.log(car.name);
+
+car.name = 'Civic'
