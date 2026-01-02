ef2a3cd00e4c8c27fa85b5b72ff52fec5705fe6a
Pedro Alves <pedromoraes.junior96@gmail.com>
2026-01-02 23:43:02 +0000

    Update advancedClasses.ts

--- advancedClasses.ts
+++ advancedClasses.ts
@@ -1,7 +1,19 @@
 class Profile {
-    constructor(private firstName: string, private lastName: string, private age: number) {}
+    constructor(private firstName: string, private lastName: string) {}
 
     get fullName() {
         return this.firstName + ' ' + this.lastName;
     }
+
+    set age(a: number) {
+        if (a < 0) {
+            throw new Error('Invalid age.');
+        }
+
+        this.age = a;
+    }
 }
+
+const profileOne = new Profile('Pedro', 'Alves');
+
+profileOne.age = 29;
